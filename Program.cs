58339e565f2b1fa10261e8b91fff050877ed219c
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-06-13 14:04:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._9._Studio_Fun_with_Quizzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // build quiz
+            Quiz quiz = new Quiz(new List<Question>());
+
+            // build multiple question format
+            List<string> firstOptions = new List<string>() { "1", "2", "3", "4",};
+            MultipleChoice firstQuestion = new MultipleChoice("2+2?", 1, firstOptions, 4);
+
+            quiz.AddQuestion(firstQuestion);
+
+            // build checkbox format
+            List<string> secondOptions = new List<string>() { "1", "2", "3", "4" };
+            List<int> answer = new List<int>() { 1, 3 };
+            CheckBox secondQuestion = new CheckBox("x+2 = abs(x+2), therefore, x = ?", 2, secondOptions, answer);
+
+            quiz.AddQuestion(secondQuestion);
+
+            // build true false format
+            TrueOrFalse thirdQuestion = new TrueOrFalse("this answer is false", 1, false);
+
+            quiz.AddQuestion(thirdQuestion);
+
+            // run quiz
+            quiz.RunQuiz();
+
+        }
+    }
+}
---
+++ Quiz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7._9._Studio_Fun_with_Quizzes
+{
+    public class Quiz
+    {
+
+        public List<Question> Questions { get; set; }
+        public double Score { get; set; }
+        private double Total { get; set; }
+
+        public Quiz(List<Question> questions)
+        {
+            Questions = questions;
+            Score = 0;
+            Total = 0;
+
+            foreach (Question qs in Questions)
+            {
+                Total += qs.PointValue;
+            }
+        }
+
+        public void AddQuestion(Question qs)
+        {
+            Questions.Add(qs);
+            Total += qs.PointValue;
+        }
+
+        public void RunQuiz()
+        {
+            foreach (Question qs in Questions)
+            {
+                Console.WriteLine("Question: ");
+                qs.DisplayQuestion();
+                qs.DisplayAnswers();
+                double points = qs.GetAnswers();
+                Console.WriteLine();
+
+
+                Score += points;
+            }
+
+            Console.WriteLine($"You answered {Score} out of {Total} questions correct. Your grade is {Score/Total * 100}%");
+        }
+
+
+    }
+}
